e6107ed93acf2ef81fea5f483ad6b2cfa04d3267
Sameer Ajmani <47604025+ajmanisameer@users.noreply.github.com>
2023-01-19 19:30:52 +0530

    server.js app.use url

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.use('/api', require('./routes/routes'));
 
+app.use(express.static('https://silver-baklava-03b1a9.netlify.app'));
+
 // Serve frontend
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static(path.join(__dirname, "client/build")));
